5ee2e03883a3914edbd5d5e4248a5cba6d7dad38
Amjad Saidam <amjadsaidama@gmail.com>
2025-10-25 14:44:56 +0100

    Corrected weight calcultions and fixed IOC (immediate or cancel) order logic, added strategy shut down functionality and general code abstraction.

--- coinbase_order_functions.py
+++ coinbase_order_functions.py
@@ -20,6 +20,7 @@ from sklearn.model_selection import train_test_split
 import datetime 
 import pandas as pd
 import numpy as np 
+from decimal import Decimal
 
 class CoinbaseTrader:
     def __init__(self, api_key, api_secret):
@@ -30,12 +31,12 @@ class CoinbaseTrader:
         """Authentication function:
         """
         try:
-            _ = self.client.get_accounts()
-            self.authenticated = True
-            return self.authenticated
+            account = self.client.get_accounts() # attempts to retreive account data to authorsie login 
+            if account is not None:
+                self.authenticated = True
         except Exception as e:
             print(e)
-            return self.authenticated
+        return self.authenticated
     
     def tickers_weight(self, tickers: list, weights: list):
         """Creates Ticker-Weight Dict 
@@ -46,11 +47,11 @@ class CoinbaseTrader:
     def get_base(self, asset: str):
         return asset.split("-")[0] 
     
-    def asset_base(self, asset: str, account_values: dict): 
+    def asset_base(self, asset: str, account_values: dict) -> float: 
         """Base Investment:
         """
         asset = self.get_base(asset)
-        return account_values[asset] # asset associted holding, e.g. BTC-USD -> BTC    
+        return account_values[asset] # asset associted holding, e.g. BTC-USD -> BTC: x  
 
     def coinbase_data(self, products, time_frame_candle, get_returns = True, freq = '1h', **kwargs): 
         """Get Close / Price Data
@@ -96,7 +97,7 @@ class CoinbaseTrader:
         
         return data  
 
-    def get_user_accounts(self):
+    def get_user_accounts(self) -> dict[float]:
         """Account Information Function
         """
         accounts = self.client.get_accounts() # all authenticated user accounts
@@ -117,61 +118,124 @@ class CoinbaseTrader:
             change_price['change'].append(percent_change)
             change_price['price'].append(price)
 
-        return change_price    
-    
-    def weighted_value(self, weight, value):
-        return abs(weight * float(value)) # maximum precision is 8 d.p. 
-    
-    def order_type(self, weight):
-        return 'SELL' if weight <= 0 else 'BUY'
-    
-    def to_base_value(self, asset, weighted_value):
-        base_asset = self.client.get_product(asset)
-        return weighted_value / float(base_asset.price)
-    
-    def base_to_quote(self, value_base):
-        return (1 / value_base)
+        return change_price   
 
-    def market_order_quantity(self, asset, weight, value, full_close = False):
-        """Get Order Quantity in Base / Quote:
+    def get_price(self, asset: str, suffix = 'GBP'):
+        """ Gets the asset price
+        asset: can be in base:quote form or base form
+        suffix: the quote currency if only the base is provided
         """
-        order_value = None
-        order_type = self.order_type(weight) # overide if full close 
-        w_value = self.weighted_value(weight, value)
+        if len(asset.split('-')) != 2: # if base e.g. 'BTC' add the GBP so we can pull a valid listed pair
+            asset = asset + '-' + suffix
 
-        base_value = self.to_base_value(asset, w_value)
+        # attempt to pull asset data, otherwise return 0 
+        try:
+            asset = self.client.get_product(asset)
+        except Exception as e:
+            return 0
+        return float(asset.price)
+    
+    def weighted_value(self, weight, total_pf_val, asset):
+        """Gets the weighted value of the asset in terms of quote (GBP)
+        value: this is the quote amount invested in the base term of the base (e.g. GBP-BTC)
+        """
+        return abs(weight * float(total_pf_val)) # maximum precision is 8 d.p. 
 
-        asset_value = self.asset_base(asset, self.get_user_accounts()) # get quantity invested in asset class
+    def to_base_value(self, asset, weighted_value) -> float:
+        """Gets weighted value in terms of asset
+        """
+        return weighted_value / self.get_price(asset) # prices will cancel leaving weight * value_invested_in_base
+    
+    def base_to_quote(self, accounts: dict[float], asset: str):
+        """Value invested in terms of quote (GBP)
+        """
+        value_base = accounts.get(self.get_base(asset), 0) # if we can not find the asset yield 0
+        return value_base * self.get_price(asset) # invested amount in quote (GBP)
+    
+    def order_type(self, weight: float):
+        '''Determines if we are buying or selling
+        '''
+        return 'SELL' if weight < 0 else 'BUY'
+    
+    def total_portfolio_value(self):
+        '''Will get the total invested portfolio value in quote (GBP)
+        '''
+        accounts = self.get_user_accounts()
+        return float(sum([self.base_to_quote(accounts, key) for key in accounts.keys() if key != 'GBP'])) # always in terms of base, transfer to quote to standerdise
 
-        base_size_sell = round(base_value, 6) # up to 8 d.p. supported for base 
-        quote_size_buy = round(w_value, 2) # up to 2 d.p. for fiate 
+    def get_real_weights(self, portfolio_tickers) -> list[float]:
+        '''Will get the weight of each asset in the portfolio
+        '''
+        accounts = self.get_user_accounts()
+        pf_tickers_base = [self.get_base(ticker) for ticker in portfolio_tickers]
+        pf_value = self.total_portfolio_value()
+        return [max(self.base_to_quote(accounts, key) / pf_value, 0) for key in pf_tickers_base]
 
-        # if full close, to close out BUY, SELL using base
-        # if full close, to close out SELL, BUY using quote (not supported for spot)
-        if not full_close:  
-            order_value = {
+    # include a get incrument function, that gets the required precision for each asset, the base_size/quote size must be an mutliple of the base/quote increment
+    def order_value_to_increment(self, asset: str, order_value: float, increment_type = 'base_increment') -> float:
+        '''Base-incruments vary per asset and IOC order type
+        '''
+        if increment_type not in ['base_increment', 'quote_increment']:
+            raise ValueError('increment type must be in list')
+
+        product = self.client.get_product(asset)
+        increment = Decimal(product[increment_type])
+        order_value = Decimal(str(order_value))
+
+        # round to integer and then scale (for quote orders increment is 1 for all assets -> order value only needs to be rounded)
+        integer = (order_value // increment) 
+        adjusted_order_value = integer * increment 
+
+        # check we meet minimum order requirements
+        min_side_value = 'quote_min_size' if increment_type == 'quote_increment' else 'base_min_size' # we do not need to deal with max value conditions unless you can buy 200BTC :)
+        min_size = Decimal(str(product[min_side_value]))
+        if adjusted_order_value < min_size: # if order amount is less than min increment skip order request
+            return 0 
+        
+        return float(adjusted_order_value)
+
+    def market_order_quantity(self, asset, weight, total_portfolio_value, full_close = False) -> list[str, float]:
+        """Gets order type and order value in base/quote
+        note if investing first time, when looping thriugh this function, base_value will raise a keyError as to_base_value() will not be able to find key in accounts.
+        To avoid this we use .get(), if the key is not found we return 0 and base_size is 0 which is valid. 
+        """
+        accounts = self.get_user_accounts()
+
+        # orders
+        if weight is not None:
+            order_type = self.order_type(weight)
+
+            w_value = self.weighted_value(weight, total_pf_val = total_portfolio_value, asset = asset)
+            base_value = self.to_base_value(asset, w_value) # for SELL, value in base (e.g. BTC)
+
+            base_size_sell = self.order_value_to_increment(asset, base_value, increment_type = 'base_increment') # SELL in base
+            quote_size_buy = self.order_value_to_increment(asset, w_value, increment_type = 'quote_increment') # BUY in quote
+
+            # if full close, to close out BUY, SELL using base
+            # if full close, to close out SELL, BUY using quote (not supported for spot)
+            order_value_standard = {
                 'base_size': str(base_size_sell) 
                 } if (order_type == 'SELL') else {
                     'quote_size': str(quote_size_buy)
                     } 
-        else:
-            base_size_close = asset_value 
-            order_value = {
-                'base_size': str(base_size_close)
-            }
             
-        return order_type, order_value
+        # full close amount 
+        asset_value = self.asset_base(asset, account_values = accounts) # for BUY, get quantity invested in asset class (float type)   
+        order_value = {'base_size': str(asset_value)} if full_close else order_value_standard
+        order_side = 'SELL' if full_close else order_type 
 
-    def create_asset_order(self, asset, weight, value, **kwargs): 
+        return order_side, order_value
+
+    def create_asset_order(self, asset, account_balance, weight, **kwargs): 
         """Order Function:
         Coinbase Advanced API python sdk function already knows what account order is sent to
-        * BUY order = quote size 
-        * SELL order = base size 
+        - BUY order = quote size 
+        - SELL order = base size 
         """
-        order_type, order_value = self.market_order_quantity(asset, weight, value)
+        order_type, order_value = self.market_order_quantity(asset, weight, total_portfolio_value = account_balance)
         try: 
             order = self.client.create_order(
-                client_order_id = str(uuid.uuid4()), # must be JSON serialisable, uuid is uniqe for each opend / closed trade
+                client_order_id = str(uuid.uuid4()), # must be JSON serialisable, uuid is unique for each opened / closed trade
                 product_id = asset, 
                 side = order_type, 
                 order_configuration= {
@@ -185,18 +249,17 @@ class CoinbaseTrader:
         return order  
     
     # function to close out positions
-    def modify_asset_order(self, asset, account_values: dict, side_close: str, full_close = False, weight_diff = None, **kwargs):
+    def modify_asset_order(self, asset, total_pf_value, weight_diff = None, full_close = False, **kwargs):
         """Close/Edit Open Positions:
-        Note: the close_position() endpoint canot be used for closing sport positions 
+        Note: the close_position() endpoint canot be used for closing spot market positions. Only valid in future markets.
         """
         order = None
-        asset_holding = self.asset_base(asset=asset, account_values = account_values) # get quote value invested in asset
-        order_modify_type, order_value = self.market_order_quantity(asset, weight_diff, asset_holding, full_close = full_close)
+        order_modify_type, order_value = self.market_order_quantity(asset, weight_diff, total_portfolio_value = total_pf_value, full_close = full_close)
         try: 
             order = self.client.create_order(
                 client_order_id = str(uuid.uuid4()), # ceate unique order id 
                 product_id = asset,
-                side = 'SELL' if full_close else side_close, # 'SELL' all holdings if full close 
+                side = order_modify_type, # 'SELL' all holdings if full close (manual overide)
                 order_configuration = {
                     'market_market_ioc': order_value 
                 }, 
@@ -207,42 +270,80 @@ class CoinbaseTrader:
         
         return order
     
-    def multi_asset_invest(self, portfolio_ticker_weights: dict, account_base = "GBP", full_close = False):
+    def multi_asset_close(self, portfolio_tickers: dict, full_close):
+        orders = []
+        if full_close:
+            for key in portfolio_tickers:
+                order = self.modify_asset_order(
+                    asset = key, 
+                    total_pf_value = None, # weights None, wo will not be passed
+                    full_close = True
+                )
+                orders.append(order)
+        
+        return orders
+        
+    def multi_asset_invest(self, portfolio_ticker_weights: dict, account_base = "GBP", shut_down = False):
         """Portfolio Rebalancing Function:
         """
         accounts = self.get_user_accounts()
         equity = accounts[account_base]
-        portfolio_value = accounts.pop(account_base) # get only invested amount / remove base account 
-        total_portfolio_value = float(sum(accounts.values()))
+        gbp_balance = accounts.pop(account_base) # get only invested amount / remove base account 
+        total_portfolio_value = self.total_portfolio_value() # initialize only once 
+        
         orders, weight_diffs = [], []
         
-        for key, new_weight in portfolio_ticker_weights.items():
-            # check if not invested, if so invest (initilise portfolio), otherwise rebalance portfolio
-            if total_portfolio_value == 0 and not full_close: # avoid issue of sending BUY orders when full_close is true
-                try: 
-                    orders.append(
-                        self.create_asset_order(asset = key, weight = new_weight, value = equity)
-                        ) # we invest once and then rebalance
-                except Exception as e:
-                    print(e)
-            else: 
-                try:
-                    base_value = accounts[self.get_base(key)]
-                    current_weight = self.base_to_quote(base_value) / total_portfolio_value # value invested in asset as fraction of total portfolio value in GBP
-                    weight_diff = new_weight - current_weight # new - old
-                    weight_diffs.append(weight_diff)
-                    side = self.order_type(weight_diff)
+        que = {}
+        if shut_down:
+            return 
+        else: # can be Any type 
+            for key, new_weight in portfolio_ticker_weights.items():
+                # check if not invested, if so invest (initilise portfolio), otherwise rebalance portfolio
+                if total_portfolio_value <= 1 or len(accounts) == 0: # possible to have currency left in each asset after closing out positions (unlikley to be alot)
+                    que = {}
+                    try: 
+                        orders.append(
+                            self.create_asset_order(asset = key, account_balance = equity, weight = new_weight) # invest weighted amount from initial balance
+                            ) # we invest once and then rebalance
+                    except Exception as e:
+                        print(e)
+                else: 
+                    try:
+                        # now modify portfolio buy taking opposite / same position in asset
+                        current_weight = max(self.base_to_quote(accounts, key) / total_portfolio_value, 0) # value invested in asset as fraction of total portfolio value in GBP, tak max to avoid divide by 0 error
+                        weight_diff = new_weight - current_weight # new - old
+                        weight_diffs.append(weight_diff)
+                        side = self.order_type(weight_diff)
 
-                    # now modify portfolio buy taking opposite / same position in asset
-                    orders.append(
-                        self.modify_asset_order(asset = key, 
-                                                account_values = accounts, # currency value invested in asset
-                                                side_close = side, 
-                                                full_close = full_close, 
-                                                weight_diff = weight_diff)
-                                                ) # for each asset / divest base if full close 
-                except Exception as e:
-                    print(e)
+                        # we have to give priority to sell orders, otherwise my may try add to a position and get an INSUFFICIENT_FUND error 
+                        if side == 'SELL': 
+                            orders.append(
+                                self.modify_asset_order(asset = key, 
+                                                        total_pf_value = total_portfolio_value,
+                                                        weight_diff = weight_diff)
+                                                        ) # for each asset/base divest if full close 
+                        else: 
+                            que[key] = {
+                                'asset': key, 
+                                'total_pf_value': total_portfolio_value,
+                                'weight_diff': weight_diff
+                            }
+                    except Exception as e:
+                        print(e)
 
-        return {'orders': orders, 'weight_diffs': weight_diffs}
+            # now we have enough fiat in GBP to complete the BUY trades, the sum to 1 constarint ensures this is possible 
+            if len(que) != 0:
+                for key, inputs in que.items():
+                    try:
+                        orders.append(
+                            self.modify_asset_order(**inputs)
+                        )
+                    except Exception as e:
+                        print(e)
+
+        return {'orders': orders, 
+                'weight_diffs': weight_diffs, 
+                'account_balance': gbp_balance, 
+                'total_spent': total_portfolio_value, 
+                'strategy_live': not shut_down} # orders will be a list of dicts 
     
