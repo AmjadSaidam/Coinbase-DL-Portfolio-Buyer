99ee7c1d2706ea2eb9eeebe5e5ab353a33632cf0
Amjad Saidam <amjadsaidama@gmail.com>
2026-09-14 17:07:45 +0100

    feat/refactor: create_data() now appends to existing dataframe in database.

--- data_loaders/data_to_sql.py
+++ data_loaders/data_to_sql.py
@@ -6,7 +6,10 @@ import datetime as dt
 
 # specifically for saving dmlstm array output
 class CreateSQLiteDatabase():
-    def __init__(self, tickers: list[str], name_database: str, name_data: str):
+    def __init__(self, 
+                 tickers: list[str], 
+                 name_database: str, 
+                 name_data: str):
         # define initial class attributes
         self.tickers = tickers 
         self.name_db = name_database
@@ -17,15 +20,12 @@ class CreateSQLiteDatabase():
         self.cur = self.con.cursor() # will allow us to exacute sql query's
 
     def create_data(self):
-        """
-        Create new data frame if one exists with the same name
-        """
+        """Create new data frame if one exists with the same name"""
         data = pd.DataFrame(columns = self.tickers)
-        data.to_sql(name = self.name, con = self.con, if_exists = 'replace', index = True) 
+        data.to_sql(name = self.name, con = self.con, if_exists = 'append', index = True) 
 
     def list_to_data(self, weights: list):
-        """
-        """
+        """"""
         data_pre_process = dict(zip(self.tickers, weights))
         self.weights = pd.DataFrame(data = data_pre_process, index = [dt.datetime.today().isoformat()])
         
@@ -33,7 +33,5 @@ class CreateSQLiteDatabase():
         self.weights.to_sql(name = self.name, con = self.con, if_exists = 'append', index = True)
         
     def query_database(self): 
-        """
-        Intended to be used seperatley from class
-        """
+        """Intended to be used seperatley from class"""
         return pd.read_sql_query(f'SELECT * FROM {self.name}', con = self.con) # return all columns of the dataframe
